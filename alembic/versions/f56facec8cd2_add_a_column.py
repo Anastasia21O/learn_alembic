6972f2f725279f6e919f4e2a0d0631a2bfed5586
Anastasia21O <virstanastya21@gmail.com>
2022-07-07 14:56:19 +0300

    (create revision) create new revision with name 'Add a column' (command: alembic revision -m 'Add a column')

---
+++ alembic/versions/f56facec8cd2_add_a_column.py
@@ -0,0 +1,24 @@
+"""Add a column
+
+Revision ID: f56facec8cd2
+Revises: 173e8fcc153e
+Create Date: 2022-07-07 14:53:30.851157
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f56facec8cd2'
+down_revision = '173e8fcc153e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
